b1fa6bab3f352de398b7e7f396a3c4f26aa4c70b
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-05-11 12:01:04 -0400

    Add deal function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,9 +1,31 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # cards =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+
+    # List.flatten(cards)
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end

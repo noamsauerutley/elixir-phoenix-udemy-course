fac17ab153541ed2e11423721a81b866daba1874
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-05-24 15:31:07 -0400

    Add set_user plug

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -21,6 +21,7 @@ defmodule Discuss.AuthController do
         |> put_flash(:info, "Welcome back!")
         |> put_session(:user_id, user.id)
         |> redirect(to: topic_path(conn, :index))
+
       {:error, _reason} ->
         conn
         |> put_flash(:error, "Error signing in")
